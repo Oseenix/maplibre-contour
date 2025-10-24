a9e47834e59e473269db59b795d5b15b66d7011d
zhoujinze <zhoujinze@gmail.com>
2025-10-25 10:42:41 +1300

    dist init

---
+++ dist/worker-dispatch.d.ts
@@ -0,0 +1,17 @@
+import { LocalDemManager } from "./local-dem-manager";
+import { Timer } from "./performance";
+import type { ContourTile, FetchResponse, IndividualContourTileOptions, InitMessage, TransferrableDemTile } from "./types";
+/**
+ * Receives messages from an actor in the web worker.
+ */
+export default class WorkerDispatch {
+    /** There is one worker shared between all managers in the main thread using the plugin, so need to store each of their configurations. */
+    managers: {
+        [id: number]: LocalDemManager;
+    };
+    init: (message: InitMessage, _: AbortController) => Promise<void>;
+    fetchTile: (managerId: number, z: number, x: number, y: number, abortController: AbortController, timer?: Timer) => Promise<FetchResponse>;
+    fetchAndParseTile: (managerId: number, z: number, x: number, y: number, abortController: AbortController, timer?: Timer) => Promise<TransferrableDemTile>;
+    fetchContourTile: (managerId: number, z: number, x: number, y: number, options: IndividualContourTileOptions, abortController: AbortController, timer?: Timer) => Promise<ContourTile>;
+    updateUrl: (managerId: number, url: string, _: AbortController) => Promise<void>;
+}
